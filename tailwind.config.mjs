541d1da4fa5f0d7d18c259efc953d36eeab9f60e
momongah <kzsidiqi@ucsd.edu>
2024-02-20 21:27:09 -0800

    added scroll button and about page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,9 @@ export default {
 	content: [
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./src/components/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js'
+		'./node_modules/flowbite/**/*.js',
+		'./src/content/projects/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./src/pages/projects/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 	],
 	theme: {
 		extend: {},
